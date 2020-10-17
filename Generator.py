d456dbe638890deb49fbf6bf62e39c2add6c3dd3
RenegadeWizard <ksychla5@gmail.com>
2020-10-17 17:38:40 +0200

    fix generator and clean up

--- Generator.py
+++ Generator.py
@@ -1,24 +1,26 @@
 import random
 from Task import Task
 
-MAX_RANGE = 30  # TODO: ??
+MAX_DURATION = 30
+MAX_START = 30
+MAX_WEIGHT = 10
 
 
 class Generator:
     def __init__(self, tasks_number):
         self.tasks = []
         for _ in range(tasks_number):
-            self.append_task()
-
-    def append_task(self):
-        p = random.randint(1, MAX_RANGE)
-        r = random.randint(1, MAX_RANGE)
-        d = random.randint(r + 1, MAX_RANGE + 1)
-        w = random.randint(1, MAX_RANGE)
-        self.tasks.append(Task(p, r, d, w))
+            self.__append_new_task()
 
     def serialize(self, file_name):
         with open(file_name, 'w') as file:
             file.write(str(len(self.tasks)) + '\n')
             for task in self.tasks:
                 file.write(str(task) + '\n')
+
+    def __append_new_task(self):
+        p = random.randint(1, MAX_DURATION)
+        r = random.randint(1, MAX_DURATION)
+        d = random.randint(r + p + 1, r + 2 * MAX_DURATION + 1)
+        w = random.randint(1, MAX_WEIGHT)
+        self.tasks.append(Task(p, r, d, w))
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from Generator import Generator
 from Validator import Validator
 
-FILE_NAME = "out/out.txt"
+OUT_FILE_NAME = "out/out.txt"
+IN_FILE_NAME = "out/in.txt"
 
 
 def main():
-    Generator(5).serialize(FILE_NAME)
-    Validator(FILE_NAME, "")
+    # Generator(5).serialize(FILE_NAME)
+    Validator(OUT_FILE_NAME, IN_FILE_NAME)
 
 
 if __name__ == '__main__':
