9fcdb17ba8d0962c007ba45921b44a35abeb4f5e
RenegadeWizard <ksychla5@gmail.com>
2020-10-20 09:35:49 +0200

    finished one part of validator

--- service/Validator.py
+++ service/Validator.py
@@ -10,18 +10,27 @@ class Validator:
         self.__check_output_file(result_file_name)
         self.execution()
 
+    def validate_result(self):
+        tasks = self.__parse_input()
+        result = self.__parse_output()
+        weight_sum = 0
+        time = 0
+        for task in result.tasks:
+            curr_task = tasks[task-1]
+            time += curr_task.r + curr_task.p
+            if time > curr_task.d:
+                weight_sum += curr_task.w
+        if weight_sum == result.u:
+            print("Correct")
+
+    def validate_algorithm(self):
+        pass  # TODO: zaimplementuj
+
     def __check_output_file(self, file_name: str):
         if file_name.rfind('.txt') > 0:
             self.execution = self.validate_result
         else:
             self.execution = self.validate_algorithm
-
-    def validate_result(self):
-        tasks = self.__parse_input()
-        result = self.__parse_output()
-
-    def validate_algorithm(self):
-        pass  # TODO: zaimplementuj
 
     def __parse_input(self) -> [Task]:
         with open(self.input, 'r') as file:
